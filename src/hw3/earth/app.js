20e5a89d07d76306d774b99fb40e7fb9a69e4bb8
CaptCaptain <55301501+CaptCaptain@users.noreply.github.com>
2026-03-10 18:49:04 +0000

    Removed tilt from Moon orbit

--- src/hw3/earth/app.js
+++ src/hw3/earth/app.js
@@ -62,7 +62,6 @@ function animate() {
 		earth.position.x - Math.sin(moonOrbitAngle) * moonOrbitalRadius;
 	moon.position.z =
 		earth.position.z + Math.cos(moonOrbitAngle) * moonOrbitalRadius;
-	moon.position.y = earth.position.y - Math.sin(moonOrbitAngle) / 5;
 	//WRITE CODE TO ROTATE THE MOON ON IT'S Y-AXIS BY 0.01 HERE
 	moon.rotation.y += 0.01;
 	renderer.render(scene, camera);
